61cda2f6a569d02eb0b0d133354df93c9e39a50d
mrpradeepp <mrpradeepp@gmail.com>
2016-10-04 19:30:48 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get('/article-one',function(req,res){
     
     res.send("request will be served now");
 });
+app.get('article-two',function(req,res){
+    res.send("Article twocomes here");
+});
+app.get('article-three',function(req,res){
+    res.send("Article three wil come here");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
